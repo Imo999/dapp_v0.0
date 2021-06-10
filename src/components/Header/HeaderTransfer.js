102c03f4ec464caed80cd3f6239fc603296c6ab1
Imo999 <rcajina.pro@gmail.com>
2021-06-11 01:15:37 +0200

    Add Component Header Transfer

---
+++ src/components/Header/HeaderTransfer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SwitchNav from '../Navigation/SwitchNav'
+
+const HeaderTransfer = () => {
+  return (
+    <header className="header-transfer">
+      <h1>Transfer</h1>
+      <p className='header-citation'>“L’obstination est le chemin de la réussite.”</p>
+      <p className='header-author'>-- Charlie Chaplin --</p>
+      <SwitchNav />
+    </header>
+  )
+}
+
+export default HeaderTransfer
