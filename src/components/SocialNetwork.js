e1b3f1251aeb325a70be3c4c02361fd8906d23c0
Imo999 <rcajina.pro@gmail.com>
2021-06-10 15:22:39 +0200

    Add SocialNetwork component

---
+++ src/components/SocialNetwork.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SocialNetwork = () => {
+
+  return (
+    <div className="social-network">
+      <ul className="logo-socialNetwork">
+        <a href="https://github.com/Imo999" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-github">{null}</a>
+        <a href="https://twitter.com/RCajina" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter">{null}</a>
+      </ul>
+    </div >
+  )
+}
+
+export default SocialNetwork
