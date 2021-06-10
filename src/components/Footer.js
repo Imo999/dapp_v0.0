e8d12ebc83f82885647028e8989d0868c1f50756
Imo999 <rcajina.pro@gmail.com>
2021-06-10 15:22:19 +0200

    add Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SocialNetwork from './SocialNetwork'
+
+const Footer = () => {
+  return (
+    <footer className='p-3 text-center bg-dark text-white '>
+      Propulse by Imoo
+      <SocialNetwork />
+    </footer>
+  )
+}
+
+export default Footer
