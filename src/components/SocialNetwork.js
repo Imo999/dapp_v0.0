9dc4694759d5d9c3294c5cc4a2eac19ebcb95817
Imo999 <rcajina.pro@gmail.com>
2021-06-10 20:56:03 +0200

    add SocialNetwork

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -6,7 +6,6 @@ const SocialNetwork = () => {
     <div className="social-network">
       <ul className="logo-socialNetwork">
         <a href="https://github.com/Imo999" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-github">{null}</a>
-        <a href="https://twitter.com/RCajina" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter">{null}</a>
       </ul>
     </div >
   )
