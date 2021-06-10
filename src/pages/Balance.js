5e69ababa2d69dc313bc2fb101f29bf3a70edd07
Imo999 <rcajina.pro@gmail.com>
2021-06-11 00:48:58 +0200

    Update Balance component

--- src/pages/Balance.js
+++ src/pages/Balance.js
@@ -1,17 +1,16 @@
-import { useContext } from 'react'
-import { Web3Context } from 'web3-hooks'
+import React from 'react'
 import Footer from '../components/Footer'
 import HeaderBalance from '../components/Header/HeaderBalance'
+import MetaMaskDashboard from '../components/MetaMask/MetaMaskDashboard'
 import '../styles/Balance.css'
 
 const Balance = () => {
-  const [web3State] = useContext(Web3Context)
 
   return (
     <>
       <HeaderBalance />
       <main className='main-balance'>
-        INTERFACE BALANCE
+        <MetaMaskDashboard />
       </main>
       <Footer />
     </>
