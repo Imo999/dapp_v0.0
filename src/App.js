44817366f09e35bd277e73377634053bf94e939b
Imo999 <rcajina.pro@gmail.com>
2021-06-11 04:44:44 +0200

    Update Typo final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Home from './Pages/Home';
-import Account from './Pages/Account';
-import Transfert from './Pages/Transfert';
-import ComingSoon from './Pages/ComingSoon';
+import Home from './Pages/Home.js';
+import Account from './Pages/Account.js';
+import Transfert from './Pages/Transfert.js';
+import ComingSoon from './Pages/ComingSoon.js';
 
 function App() {
   return (
